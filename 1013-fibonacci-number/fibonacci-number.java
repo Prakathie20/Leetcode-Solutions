1cb33f4fce390ddd005d30e20b67c433e2e5814a
Prakathie.S <153730601+Prakathie20@users.noreply.github.com>
2026-04-01 22:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (24.12%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n<=1)
+        return n;
+        int p1=1;
+        int p2=0;
+
+        for(int i=2;i<=n;i++)
+        {
+            int c = p1+p2;
+            p2=p1;
+            p1=c;
+        }
+        return p1;
+    }
+}
